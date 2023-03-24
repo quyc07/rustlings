cad7474669b2a697e8b8f32a936d508bd5c7bd95
quyuanchao <quyuanchao@meituan.com>
2023-03-24 18:57:40 +0800

    do exercise error

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -17,7 +17,6 @@
 // one is a lot shorter!
 // Execute `rustlings hint errors2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
@@ -25,8 +24,9 @@ pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
     let qty = item_quantity.parse::<i32>();
+    if qty.is_err() { return qty; }
 
-    Ok(qty * cost_per_item + processing_fee)
+    Ok(qty.unwrap() * cost_per_item + processing_fee)
 }
 
 #[cfg(test)]
--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -4,7 +4,6 @@
 // Why not? What should we do to fix it?
 // Execute `rustlings hint errors3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
@@ -12,20 +11,20 @@ fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
-
-    if cost > tokens {
-        println!("You can't afford that many!");
-    } else {
-        tokens -= cost;
-        println!("You now have {} tokens.", tokens);
+    if let cost = total_cost(pretend_user_input).unwrap() {
+        if cost > tokens {
+            println!("You can't afford that many!");
+        } else {
+            tokens -= cost;
+            println!("You now have {} tokens.", tokens);
+        }
     }
 }
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>()?;
+    Ok(item_quantity.parse::<i32>()? * cost_per_item + processing_fee)
 
-    Ok(qty * cost_per_item + processing_fee)
+    // Ok(qty * cost_per_item + processing_fee)
 }
