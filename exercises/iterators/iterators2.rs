bb5901de2c7b59b02719ce98f20d912d2c906436
quyc <quyc07@163.com>
2023-03-28 00:03:42 +0800

    do exercise iter

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -3,7 +3,6 @@
 // can offer. Follow the steps to complete the exercise.
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 // Step 1.
 // Complete the `capitalize_first` function.
@@ -12,7 +11,11 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => {
+            let mut first = first.to_uppercase().to_string();
+            first.push_str(c.as_str());
+            first
+        }
     }
 }
 
@@ -21,7 +24,12 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    let mut vec: Vec<String> = Vec::new();
+    let mut iter = words.iter();
+    while let Some(word) = iter.next() {
+        vec.push(capitalize_first(*word));
+    }
+    vec
 }
 
 // Step 3.
@@ -29,7 +37,12 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    let mut string = String::new();
+    let mut iter = words.iter();
+    while let Some(word) = iter.next() {
+        string.push_str(&capitalize_first(*word));
+    }
+    string
 }
 
 #[cfg(test)]
--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -1,7 +1,6 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -13,6 +12,16 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    // for 循环
+    // if num == 0 { 1 } else {
+    //     let mut result = 1;
+    //     for i in 1..num + 1 {
+    //         result *= i;
+    //     }
+    //     result
+    // }
+    // rustful
+    (1..num + 1).into_iter().fold(1, |i, x| i * x)
 }
 
 #[cfg(test)]
@@ -28,6 +37,7 @@ mod tests {
     fn factorial_of_1() {
         assert_eq!(1, factorial(1));
     }
+
     #[test]
     fn factorial_of_2() {
         assert_eq!(2, factorial(2));
